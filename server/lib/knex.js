aace69bc2a7ca401c073a9ba3246ed6815d5f11d
ovh fe <ovhfe@aihello.com>
2024-08-27 15:20:50 +0000

    Connection pooling

--- server/lib/knex.js
+++ server/lib/knex.js
@@ -20,6 +20,14 @@ const knex = require('knex')({
             'DATETIME'
         ]
     },
+ pool: {
+        min: 2,  // Minimum number of connections in the pool
+        max: 20, // Maximum number of connections in the pool
+        idleTimeoutMillis: 30000, // Close idle connections after 30 seconds
+        createTimeoutMillis: 30000, // Timeout for connection creation
+        acquireTimeoutMillis: 30000, // Timeout for connection acquisition
+        propagateCreateError: false // Avoid propagating connection errors, useful for pool warm-up
+    },
     migrations: {
         directory: path.join(__dirname, '..', 'setup', 'knex', 'migrations')
     }
